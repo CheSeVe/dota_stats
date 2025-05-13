e376e9f33f6e76bb2f2a86474b1885c3819f74db
CheSeVe <z.groodson@gmail.com>
2025-05-13 11:30:34 +0400

    Initial commit: start pages and logic

---
+++ src/main/java/ru/CheSeVe/lutiy_project/LutiyProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.CheSeVe.lutiy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutiyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutiyProjectApplication.class, args);
+	}
+
+}
